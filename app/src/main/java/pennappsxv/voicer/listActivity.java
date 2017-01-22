07232c74d9fe5580925bfc9bb8a1c859a0a343df
Jeffrey Chang <jeffreychang@Jeffreys-MacBook-Pro.local>
2017-01-22 03:02:23 -0500

    fuck

--- app/src/main/java/pennappsxv/voicer/listActivity.java
+++ app/src/main/java/pennappsxv/voicer/listActivity.java
@@ -1,19 +1,83 @@
 package pennappsxv.voicer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import android.util.Log;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class listActivity extends AppCompatActivity {
-
+    private String TAG = "TAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+    StringRequest postRequest = new StringRequest(
+            Request.Method.POST,
+            APIKeys.priaid_authservice_url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    // response
+                    Log.d("Response", response);
+                }
+            },
+            new Response.ErrorListener()
+            {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // error
+                    Log.d("Error.Response", error.toString());
+
+                }
+            }
+    ) {
+        @Override
+        protected Map<String, String> getParams()
+        {
+            Map<String, String>  params = new HashMap<String, String>();
+            try {
+                params.put("Authorization", "Bearer " + URLEncoder.encode(APIKeys.priaid_authservice_url, "UTF-8"));
+            }
+            catch(UnsupportedEncodingException e) {
+                Log.d(TAG, "Unsupported encoding");
+            }
+
+
+            return params;
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -25,6 +89,8 @@ public class listActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
             }
         });
     }
