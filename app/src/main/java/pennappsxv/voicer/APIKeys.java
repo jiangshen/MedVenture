294f20f03f2d391727581dec3c1844917749bfa8
Jeffrey Chang <jeffreychang@Jeffreys-MacBook-Pro.local>
2017-01-22 01:38:32 -0500

    second activity

---
+++ app/src/main/java/pennappsxv/voicer/APIKeys.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jeffreychang on 1/22/17.
+ */
+package pennappsxv.voicer;
+
+public class APIKeys {
+    public static String apiMedicUserName = "jeffrey.chang75@gmail.com";
+    public static String apiMedicPassword = "s7Y9Ntk5ETm6d2W8B";
+    public static String priaid_authservice_url = "https://sandbox-authservice.priaid.ch/login";
+    public static String priaid_healthservice_url = "https://sandbox-healthservice.priaid.ch";
+
+}
---
+++ app/src/main/java/pennappsxv/voicer/Diagnostic.java
@@ -0,0 +1,14 @@
+package pennappsxv.voicer;
+
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by jeffreychang on 1/22/17.
+ */
+
+public class Diagnostic {
+    RequestQueue queue = Volley.newRequestQueue(this);
+    public String getToken() {
+    }
+
+}
